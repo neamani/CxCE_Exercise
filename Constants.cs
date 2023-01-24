c31c8823013d4cbb7e16a4afdeb5494314723002
neamani <99293285+neamani@users.noreply.github.com>
2023-01-24 15:10:07 +0200

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -1,12 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
 
 namespace CxCE_Demo
 {
+class PasswordExample
+{
+    public static string GetPassword()
+    {
+        return ConfigurationManager.AppSettings["password"];
+    }
+}
+
     class Constants
     {
-        public static string DB_PASSWORD = "Checkmarx123!";
+        public static string DB_PASSWORD = PasswordExample.GetPassword();
     }
 }
